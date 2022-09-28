11d2ef7c793f174acd30c055fd1aa595e453036a
oolralra <oolralra@gmail.com>
2022-09-28 02:17:19 +0000

    test

---
+++ pybo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Question
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject','content']
+
+admin.site.register(Question, QuestionAdmin)
